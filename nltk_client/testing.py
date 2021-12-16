8552db674978642a8cc54207f3601c66f91a2454
Thomas Osterland <highway.ita07@web.de>
2021-12-16 13:18:27 +0100

    added tests and stuff

--- nltk_client/testing.py
+++ nltk_client/testing.py
@@ -24,15 +24,34 @@
 from textidote import get_text, complete_check, translate_indices, set_advice
 from grammark import check_wordiness
 
-text_as_lines = get_text()
-print(text_as_lines)
-text = " ".join(text_as_lines)
-print(text_as_lines)
-res = check_wordiness(text_as_lines)
+text = get_text()
+print(text)
+res = check_wordiness(text)
 
 for r in res["findings"]:
     indices = translate_indices(r["start_pos"], r["end_pos"])
     print(indices)
-    set_advice(indices[0], r["remark"], indices[1], indices[2])
+    print(text[r["start_pos"] : r["end_pos"]])
+    #set_advice(indices[0], r["remark"], indices[1], indices[2])
+    set_advice(indices[0], f"wordiness: {r['remark']}",r["start_pos"], r["end_pos"])
 
 complete_check()
+
+##########################################
+# Test translate_indices
+##########################################
+
+#test_list = ['', 'some text', ' and everything else', '', '', 'something']
+#test_joined = " ".join(test_list)
+#print(test_list)
+#print(test_joined)
+#print("Spaces replaced by %:")
+#print(test_joined.replace(' ', '%'))
+#
+#start_pos = 16
+#end_pos = 26
+#
+#print(f"start_pos: {start_pos}, end_pos: {end_pos}, substring: {test_joined[start_pos:end_pos]}")
+#indices = translate_indices(start_pos, end_pos, test_list)
+#print(f"Translated indices: {indices}")
+#print(f"Extracted in list:{test_list[indices[0]][indices[1]:indices[2]]}")
